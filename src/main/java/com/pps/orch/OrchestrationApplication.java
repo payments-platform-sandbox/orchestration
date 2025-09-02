d67200c0eda6417f6d365ac3c2151cefa69b8fe8
Satnam Walia <42784435+satnam-walia@users.noreply.github.com>
2025-09-02 15:26:33 -0400

    init project structure + pom generation

---
+++ src/main/java/com/pps/orch/OrchestrationApplication.java
@@ -0,0 +1,11 @@
+package com.pps.orch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestrationApplication.class, args);
+    }
+}
